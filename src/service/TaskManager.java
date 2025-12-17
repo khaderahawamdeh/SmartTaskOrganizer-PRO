a84ed5027cecea532733b63c6a8f5eac6cb54514
khaderahawamdeh <you@example.com>
2025-12-17 23:12:46 +0200

    STO-14 Load tasks automatically from file

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,15 +1,20 @@
 package service;
 
 import model.Task;
+import model.TaskStatus;
 import model.Priority;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
 
 
 public class TaskManager {
@@ -18,8 +23,11 @@ public class TaskManager {
     private int nextId = 1;
 
     public TaskManager() {
-        tasks = new ArrayList<>();
-    }
+    tasks = new ArrayList<>();
+    loadTasksFromFile();
+}
+
+
 
     public Task addTask(String title, String description,
             LocalDate deadline, Priority priority) {
@@ -78,6 +86,42 @@ public class TaskManager {
         System.out.println("Error saving tasks to file.");
     }
 }
+
+
+private void loadTasksFromFile() {
+    File file = new File("data/tasks.txt");
+    if (!file.exists()) {
+        return;
+    }
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("\\|");
+
+            int id = Integer.parseInt(parts[0]);
+            String title = parts[1];
+            String description = parts[2];
+            LocalDate deadline = LocalDate.parse(parts[3]);
+            Priority priority = Priority.valueOf(parts[4]);
+            TaskStatus status = TaskStatus.valueOf(parts[5]);
+
+            Task task = new Task(id, title, description, deadline, priority);
+            task.setStatus(status);
+
+            tasks.add(task);
+
+            if (id >= nextId) {
+                nextId = id + 1;
+            }
+        }
+    } catch (Exception e) {
+        System.out.println("Error loading tasks from file.");
+    }
+}
+
+
 
    public boolean editTaskPartial(int id,
         String newTitle,
